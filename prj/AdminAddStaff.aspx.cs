ba7528289bdd081a651c098b61c4c9694c65d336
oguzhankrrr <62333514+oguzhankrrr@users.noreply.github.com>
2020-04-02 16:59:28 +0300

    AdminAddStaffPage eklendi

--- prj/AdminAddStaff.aspx.cs
+++ prj/AdminAddStaff.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Coiffeur_Appointment_System
 {
-    public partial class adminProcedurePage : System.Web.UI.Page
+    public partial class AdminAddStaff : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
